805b63f5161301a6e91a0c515a8322b4e72bb3bf
Luydmyla <lytic.ok@gmail.com>
2022-05-11 01:23:56 +0300

    fixed some styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { App } from 'components/App';
 import App from './components/App';
+import 'modern-normalize/modern-normalize.css';
+
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
